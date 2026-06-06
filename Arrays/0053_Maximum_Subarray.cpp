eb0783b9169e1c9653d395355edb35429b0e17e2
Vishnu A <137207623+vishnuu123pl@users.noreply.github.com>
2026-06-06 17:42:23 +0530

    Update 0053_Maximum_Subarray.cpp

--- Arrays/0053_Maximum_Subarray.cpp
+++ Arrays/0053_Maximum_Subarray.cpp
@@ -1,16 +1,34 @@
+/*
+LeetCode 53 - Maximum Subarray
+
+Given an integer array nums, find the contiguous subarray
+with the largest sum and return its sum.
+
+Example:
+Input: [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+
+Approach:
+Kadane's Algorithm
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int cs = 0, ms = INT_MIN;
+        int currSum = 0;
+        int maxSum = nums[0];
 
-        for (int val : nums) {
-            cs += val;
-            ms = max(ms, cs);
+        for (int num : nums) {
+            currSum += num;
+            maxSum = max(maxSum, currSum);
 
-            if (cs < 0)
-                cs = 0;
+            if (currSum < 0)
+                currSum = 0;
         }
 
-        return ms;
+        return maxSum;
     }
 };
